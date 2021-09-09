c53795b1ec5aa5b52566c75726ba29b318140854
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-09 13:44:35 -0500

    creación de funcion calcular iva en la clase product iva (ex4)

--- src/main/java/com/sofkau/exercises/Four_product_iva.java
+++ src/main/java/com/sofkau/exercises/Four_product_iva.java
@@ -1,4 +1,19 @@
 package com.sofkau.exercises;
 
+import java.util.Scanner;
+
 public class Four_product_iva {
+
+    public void calculate_iva(){
+
+        double price;
+        double final_price;
+        String price_string;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("En este ejercicio calcula el IVA al precio neto de un producto");
+        System.out.print("Por favor ingrese el precio del producto: \n" +
+                " ");
+        price_string = scanner.next();
+        price = Double.parseDouble(price_string);
+    }
 }
--- src/main/java/com/sofkau/exercises/main.java
+++ src/main/java/com/sofkau/exercises/main.java
@@ -11,6 +11,7 @@ public class main {
         One_greater one_greater = new One_greater();
         Two_num_comparison two_num_comparison = new Two_num_comparison();
         Three_circle_area three_circle_area = new Three_circle_area();
+        Four_product_iva four_product_iva = new Four_product_iva();
 
         while(!salir){
             System.out.print("Ingrese el número del ejercicio que desea probar: : \n" +
@@ -26,6 +27,7 @@ public class main {
                 case "1": one_greater.isGreater();
                 case "2": two_num_comparison.comparison();
                 case "3": three_circle_area.calculate_radius();
+                case "4": four_product_iva.calculate_iva();
                 case "19": salir = true;
 
             }
