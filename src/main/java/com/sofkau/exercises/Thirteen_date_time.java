712a7a5de71b434f1f81f3db8c3cf22156eede3d
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-09 21:50:14 -0500

    Ex13. creada la funcion data time actual vacía

---
+++ src/main/java/com/sofkau/exercises/Thirteen_date_time.java
@@ -0,0 +1,8 @@
+package com.sofkau.exercises;
+
+public class Thirteen_date_time {
+
+    public void date_time_actual(){
+
+    }
+}
--- src/main/java/com/sofkau/exercises/main.java
+++ src/main/java/com/sofkau/exercises/main.java
@@ -20,6 +20,7 @@ public class main {
         Ten_delete_string_spaces ten_delete_string_spaces = new Ten_delete_string_spaces();
         Eleven_string_length eleven_string_length = new Eleven_string_length();
         Twelve_equal_words twelve_equal_words = new Twelve_equal_words();
+        Thirteen_date_time thirteen_date_time = new Thirteen_date_time();
 
         while(!salir){
             System.out.print("Ingrese el número del ejercicio que desea probar: : \n" +
@@ -35,6 +36,7 @@ public class main {
                     "10) Eliminar espacios en una cadena de texto \n" +
                     "11) Contar la cantidad de carácteres y vocales en una cadena de texto \n" +
                     "12) ¿Palabras iguales? \n" +
+                    "12) Consultar fecha y hora actual \n" +
                     "19) salir\n" +
                     "");
 
@@ -52,6 +54,7 @@ public class main {
                 case "10": ten_delete_string_spaces.delete_string_spaces(); break;
                 case "11": eleven_string_length.string_length(); break;
                 case "12": twelve_equal_words.equal_words(); break;
+                case "13": thirteen_date_time.date_time_actual(); break;
                 case "19": salir = true;  break;
                 default:
                     System.out.println("Selección inválida, intente nuevamente");
