681a41e4848c190749bff377f67d2a595f10f508
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-10 07:58:30 -0500

    calcular el imc, si es mayor de edad e imprimir el objeto)

--- src/main/java/com/sofkau/exercises/Controller.java
+++ src/main/java/com/sofkau/exercises/Controller.java
@@ -10,6 +10,12 @@ public class Controller {
 
     public void excersice16_methods() {
 
+        class error extends Exception{
+            public error(String mensaje){
+                super(mensaje);
+            }
+        }
+
         String name;
         String sex = "";
         String sex_temp;
@@ -19,19 +25,46 @@ public class Controller {
         String input_string;
         String input[];
         double imc;
-        Persona persons[] = new Persona[100];
+        boolean legal_age;
+        Persona persons[] = new Persona[3];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escribir: nombre, edad, sexo (Masculino,Femenino), peso (Kg), altura (m)");
-        input_string = scanner.nextLine();
-        input = input_string.split(",");
-        name = input[0];
-        age = Integer.parseInt(input[1]);
-        sex_temp = input[2];
+        System.out.println("Escribir: nombre");
+        name = scanner.nextLine();
+        System.out.println("Edad");
+        try{age = Integer.parseInt(scanner.nextLine());}
+        catch (Exception e) {System.out.println("Ingrese la edad nuevamente");
+            age = Integer.parseInt(scanner.nextLine());}
+        System.out.println("Sexo (Masculino, Femenino)");
+        try{
+            sex_temp = scanner.nextLine();
+            if(sex_temp.equalsIgnoreCase("Masculino")|| sex_temp.equalsIgnoreCase("Femenino")){
+                System.out.println("entro aqui");}
+            else{
+                System.out.println("entro al else");
+                error e = new error("");
+                throw e;
+            }
+        }
+        catch (Exception excepcion) {
+            excepcion.printStackTrace();
+            System.out.println("Ingrese Masculino o femenino solamente");
+            sex_temp = scanner.nextLine();
+        }
         if(sex_temp.equalsIgnoreCase("Masculino")){sex = "M";}
         else if(sex_temp.equalsIgnoreCase("Femenino")){sex = "F";}
-        weight = Double.parseDouble(input[3]);
-        height = Double.parseDouble(input[4]);
+        System.out.println("Peso (Kg)");
+        try{weight = Double.parseDouble(scanner.nextLine());}
+        catch (Exception e) {
+            System.out.println("Ingrese el peso nuevamente");
+            weight = Double.parseDouble(scanner.nextLine());
+        }
+        System.out.println("Altura (m)");
+        try{height = Double.parseDouble(scanner.nextLine());}
+        catch (Exception e){
+            System.out.println("Ingrese altura nuevamente");
+            height = Double.parseDouble(scanner.nextLine());
+        }
 
         Persona persona1 = new Persona(name,age,sex,weight,height);
         Persona persona2 = new Persona(name,age,sex);
@@ -50,35 +83,27 @@ public class Controller {
         persons[1] = persona2;
         persons[2] = persona3;
 
-        for(int i = 0; i < persons.length-1; i++ ){
+        System.out.println(persons.length);
+
+        for(int i = 0; i < persons.length; i++ ){
             imc =  persons[i].calculate_IMC();
-            System.out.println(persons[i]);
-            System.out.println(imc);
             if(imc == -1){
                 System.out.println(persons[i].getNombre() + " Se encuentra por debajo del peso ideal");
             }else if(imc == 0){
                 System.out.println(persons[i].getNombre() + " Se encuentra en el peso ideal");
             }else if(imc == 1){
-                System.out.println(persons[i].getNombre() + " Se encuentra por encima del peso ideal (Sobrepeso");
+                System.out.println(persons[i].getNombre() + " Se encuentra por encima del peso ideal");
             }
+            legal_age = persons[i].is_legal_age();
+            if(legal_age == true){
+                System.out.println(persons[i].getNombre() + " Es mayor de edad");
+            }else if(legal_age == false){
+                System.out.println(persons[i].getNombre() + " No es mayor de edad");
+            }
+            System.out.println(persons[i].toString());
         }
 
 
-
-
-//        double imc;
-//        boolean legal_age;
-//        String object;
-//        imc = persona.calculate_IMC();
-//        System.out.println("El IMC es: " + imc);
-//        legal_age = persona.is_legal_age();
-//        System.out.println("Es legal la edad?:" + legal_age);
-//        object = persona.toString();
-//        System.out.println(object);
-
     }
-
-
-
 }
 
