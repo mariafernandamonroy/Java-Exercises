26aaf765c2847f8827f062b1760c4fcd6d1caf0b
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-09 21:12:06 -0500

    se creó la funcion equal words completada y probada

--- src/main/java/com/sofkau/exercises/Twelve_equal_words.java
+++ src/main/java/com/sofkau/exercises/Twelve_equal_words.java
@@ -1,9 +1,31 @@
 package com.sofkau.exercises;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Twelve_equal_words {
 
     public void equal_words(){
 
+        String word1;
+        String word2;
+        String string_words;
+        String words[];
+        String diferences = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Este programa identifica la diferencia entre dos palabras");
+        System.out.println("Ingrese dos palabras separadas por coma (ex. casa,perro): ");
 
+        string_words = scanner.nextLine();
+        words = string_words.split(",");
+        word1 = words[0];
+        word2 = words[1];
+        if (word1.equalsIgnoreCase(word2)){
+            System.out.println("Las palabras " + string_words + " son iguales");
+        }else{
+            diferences = word_diferences(word1,word2);
+            System.out.println("Las palabras son diferentes: " + diferences);
+        }
     }
+
 }
